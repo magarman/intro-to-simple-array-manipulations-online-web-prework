eedf8d59aead5593f738f2e25ce20a68bff0010b
Mariana Garman <garman.mariana@gmail.com>
2019-03-16 15:10:05 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,47 @@
+def using_push(array, string)
+  ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"].push("Niger")
+end
+
+def using_unshift(array, string)
+  ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"].unshift("Brooklyn Heights")
+end
+
+def using_pop(array)
+  ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"].pop
+end
+
+def pop_with_args(array)
+  ["Danny T.", "Tyrion Lannister", "Stable Boy", "Sandor Clegane"].pop(2)
+end
+
+def using_shift(array)
+  ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"].shift
+end
+
+def shift_with_args(array)
+
+end
+
+def using_concat(array1, array2)
+  
+end
+
+def using_insert(array, element)
+
+end
+
+def using_uniq(array)
+  
+end
+
+def using_flatten(array)
+  
+end
+
+def using_delete(array, string)
+  
+end
+
+def using_delete_at(array, integer)
+
+end
